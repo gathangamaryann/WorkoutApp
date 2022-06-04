5fc3445546bb293d5835a9c3be33f822db7346e9
gathangamaryann <maryanngathanga@gmail.com>
2022-06-04 08:20:34 +0300

    first commit

--- app/src/main/java/dev/maryann/workoutlog/SplashActivity.kt
+++ app/src/main/java/dev/maryann/workoutlog/SplashActivity.kt
@@ -1,11 +1,15 @@
 package dev.maryann.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+     startActivity(Intent(this,LoginActivity::class.java))
+//        startActivity(intent)
+           finish()
     }
 }
